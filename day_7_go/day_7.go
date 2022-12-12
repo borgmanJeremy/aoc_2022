23db8fc751eb95e65eaa4e22ad4d5d60b5705bdb
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2022-12-12 15:01:00 -0600

    This approach is not working

--- day_7_go/day_7.go
+++ day_7_go/day_7.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
+
+	"github.com/dominikbraun/graph"
+	"github.com/dominikbraun/graph/draw"
 )
 
 func checkError(err error) {
@@ -13,30 +19,158 @@ func checkError(err error) {
 	}
 }
 
-func parseCommand(input string) {
-	tokens := strings.Split(input, " ")
+type KeyValue struct {
+	Key   string
+	Value int
+}
 
-	if tokens[1] == "ls" {
-
-	} else if tokens[1] == "cd" {
-	} else {
-		log.Fatal("Invalid command")
+func sortMap(myMap map[string]int) map[string]int {
+	pairs := make([]KeyValue, len(myMap))
+	i := 0
+	for k, v := range myMap {
+		pairs[i] = KeyValue{k, v}
+		i++
 	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].Value > pairs[j].Value
+	})
+
+	sortedMap := make(map[string]int)
+	for _, pair := range pairs {
+		sortedMap[pair.Key] = pair.Value
+	}
+	return sortedMap
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+type Node struct {
+	Name string
+	Id   int
+	Size int
+	Type string
+}
+
+func nodeHash(n Node) int {
+	return n.Id
+}
+
+func findParent(g graph.Graph[string, Node], currentNode string) string {
+	am, _ := g.AdjacencyMap()
+	for _, node := range am[currentNode] {
+		prop := node.Properties.Attributes
+		if prop["type"] == "parent" {
+			return node.Target
+		}
+	}
+	file, _ := os.Create("./mygraph.gv")
+	_ = draw.DOT(g, file)
+
+	log.Fatal("Could not find Parent")
+	return ""
+}
+
+func directorySize(g graph.Graph[string, Node], currentNode string) int {
+	am, _ := g.AdjacencyMap()
+	size := 0
+	for _, node := range am[currentNode] {
+		vertex, _ := g.Vertex(node.Target)
+		prop := node.Properties.Attributes
+
+		if prop["type"] == "child" {
+			if vertex.Type == "folder" {
+				size += directorySize(g, node.Target)
+			} else {
+				size += vertex.Size
+			}
+		}
+	}
+	return size
 }
 
 func main() {
-	instructions, err := os.Open("input/sample.txt")
+	instructions, err := os.Open("input/input.txt")
 	checkError(err)
-
 	defer instructions.Close()
 
+	id := 0
+	g := graph.New(nodeHash, graph.Directed(), graph.Acyclic())
+	g.AddVertex(Node{"/", id, 0, "folder"})
+	id += 1
+
+	currentNodeId := 0
+	dirList := []string{}
+	dirList = append(dirList, "/")
 	scanner := bufio.NewScanner(instructions)
+
+	count := 0
+	// Buliding Tree
 	for scanner.Scan() {
 		line := scanner.Text()
+		fmt.Println(count, ":", line)
+
+		count += 1
 		if line[0] == '$' {
-			parseCommand(line)
+			tokens := strings.Split(line, " ")
+			if tokens[1] == "ls" {
+
+			} else if tokens[1] == "cd" {
+				if tokens[2] == ".." {
+					parentId := findParent(g, currentNodeId)
+					currentNodeId = parentId
+				} else {
+					if tokens[2] != "/" {
+						currentNode = tokens[2]
+					}
+				}
+			} else {
+				log.Fatal("Invalid command")
+			}
+		} else {
+			// handle files and dirs
+			tokens := strings.Split(line, " ")
+			if tokens[0] == "dir" {
+				if !contains(dirList, tokens[1]) {
+					dirList = append(dirList, tokens[1])
+
+					g.AddVertex(Node{tokens[1], id, 0, "folder"})
+					id += 1
+				}
+
+				g.AddEdge(currentNode, tokens[1], graph.EdgeAttribute("type", "child"))
+				g.AddEdge(tokens[1], currentNode, graph.EdgeAttribute("type", "parent"))
+			} else {
+				weight, _ := strconv.Atoi(tokens[0])
+				g.AddVertex(Node{tokens[1], weight, "file"})
+				g.AddEdge(currentNode, tokens[1], graph.EdgeAttribute("type", "child"))
+				g.AddEdge(tokens[1], currentNode, graph.EdgeAttribute("type", "parent"))
+			}
 		}
-		// fmt.Println(line)
 	}
+
+	dirSize := make(map[string]int)
+	for _, dir := range dirList {
+		dirSize[dir] = directorySize(g, dir)
+	}
+	part_1 := 0
+	for _, dir := range dirList {
+		if dirSize[dir] <= 100000 {
+			fmt.Println(dir, dirSize[dir])
+			part_1 += dirSize[dir]
+		}
+	}
+
+	fmt.Println("Part 1: ", part_1)
+
+	//file, _ := os.Create("./mygraph.gv")
+	//_ = draw.DOT(g, file)
 
 }
--- day_7_go/go.mod
+++ day_7_go/go.mod
@@ -1,3 +1,7 @@
 module example.com/day_7
 
 go 1.19
+
+require (
+	github.com/dominikbraun/graph v0.15.0 // indirect
+)
