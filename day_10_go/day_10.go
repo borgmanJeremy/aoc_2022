8dd51285acf254e62d1278698a143eb45a875156
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2022-12-13 16:33:53 -0600

    working on it

--- day_10_go/day_10.go
+++ day_10_go/day_10.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func checkError(err error) {
@@ -13,17 +14,89 @@ func checkError(err error) {
 	}
 }
 
+type OpCode int
+
+const (
+	noop OpCode = iota
+	addx
+)
+
+func decodeOpCode(input string) OpCode {
+	switch input {
+	case "addx":
+		return addx
+	case "noop":
+		return noop
+	}
+	log.Fatal("Unknown OpCode")
+	return 0
+}
+
+type CPU struct {
+	clock int
+	regX  int
+
+	clockQueue int
+	pendingAdd bool
+
+	instructions []string
+}
+
+func makeCPU(instructions []string) CPU {
+	cpu.clock = 0
+	cpu.regX = 0
+	cpu.clockQueue = 0
+	cpu.pendingAdd = false
+	copy(cpu.instructions, instructions)
+	return cpu
+}
+
+func (cpu *CPU) decode(instruction string) {
+	tokens := strings.Split(instruction, " ")
+	opCode := decodeOpCode(tokens[0])
+
+	switch opCode {
+	case addx:
+		number, _ := strconv.Atoi(tokens[1])
+		cpu.addx(number)
+	case noop:
+		cpu.noop()
+	}
+
+}
+
+func (cpu *CPU) noop() {
+}
+func (cpu *CPU) addx(input int) {
+}
+
+func (cpu *CPU) tick() {
+	cpu.clock += 1
+
+	if cpu.pendingAdd {
+		if cpu.clockQueue > 0 {
+			cpu.clockQueue -= 1
+		} else {
+			cpu.regX += 1
+			cpu.pendingAdd = false
+		}
+	}
+}
+
 func main() {
-	instructions, err := os.Open("input/input.txt")
+	instructions, err := os.Open("input/sample.txt")
 	checkError(err)
 
 	defer instructions.Close()
 
 	scanner := bufio.NewScanner(instructions)
+	instructionList := make([]string, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
-
+		instructionList = append(instructionList, line)
 	}
+
+	// create new cpu
+	cpu := new(CPU)
 
 }
